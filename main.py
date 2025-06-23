40eaad8cc6c0fd4fdff7c587a978663d935cb048
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-23 13:51:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -190,13 +190,13 @@ async def upload(bot: Client, m: Message):
                         else:
                             await m.reply_text(f"❌ Failed to download PDF (Status Code: {response.status_code})\nURL: {url}")
                             continue
-                        except FloodWait as e:
-                            await m.reply_text(str(e))
-                            time.sleep(e.x)
-                            continue
-                        except Exception as e:
-                            await m.reply_text(f"❌ PDF download failed:\n{str(e)}\n**URL**: `{url}`")
-                            continue
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue
+                    except Exception as e:
+                        await m.reply_text(f"❌ PDF download failed:\n{str(e)}\n**URL**: `{url}`")
+                        continue
                 else:
                     Show = f"**⥥ 🄳🄾🅆🄽🄻🄾🄰🄳🄸🄽🄶⬇️⬇️... »**\n\n**📝Name »** `{name}\n❄Quality » {raw_text2}`\n\n**🔗URL »** `{url}`"
                     prog = await m.reply_text(Show)
